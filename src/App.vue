126cdae9f47840dc5e4e5b868e31b4e43a4c9782
SimoneBenato <sb270298@gmail.com>
2021-10-25 09:55:18 +0200

    La mia prima app con vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <Title />
+    <Jumbotron />
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Jumbotron from './components/Jumbotron.vue'
+import Title from './components/Title.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Title,
+    Jumbotron,
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss"> 
+
 </style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="https://image.posterlounge.it/images/l/1894548.jpg" alt="superman">
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
